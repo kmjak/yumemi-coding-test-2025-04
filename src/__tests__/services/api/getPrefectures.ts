942a5d6f4760d1791f0de1bece3361cbad82c93c
kmjak <tymkoron1221@icloud.com>
2025-04-06 03:49:01 +0900

    update: responseが配列じゃないテストケースを作成

--- src/__tests__/services/api/getPrefectures.ts
+++ src/__tests__/services/api/getPrefectures.ts
@@ -42,4 +42,19 @@ describe('getPrefectures', () => {
     // レスポンスがfalseであることを確認
     expect(response).toBe(false);
   });
+
+  // テストケース: レスポンスデータが配列でない場合
+  test('レスポンスデータが配列でない場合はfalseを返す', async (): Promise<void> => {
+    // モックのfetch関数を定義
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: async (): Promise<unknown> => ({ message: 'Invalid data' }),
+    });
+
+    // getPrefectures関数を実行
+    const response: Prefecture[] | false = await getPrefectures();
+
+    // レスポンスがfalseであることを確認
+    expect(response).toBe(false);
+  });
 });
