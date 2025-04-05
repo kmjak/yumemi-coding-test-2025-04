de079431177d4a56ce2945cd0059c352abf92544
kmjak <tymkoron1221@icloud.com>
2025-04-06 03:45:30 +0900

    add: responseがokではない場合のテストケースを用意

--- src/__tests__/services/api/getPrefectures.ts
+++ src/__tests__/services/api/getPrefectures.ts
@@ -26,4 +26,20 @@ describe('getPrefectures', () => {
       },
     ]);
   });
+
+  // テストケース: レスポンスがOKでない場合
+  test('レスポンスがOKでない場合はfalseを返す', async (): Promise<void> => {
+    // モックのfetch関数を定義
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: false,
+      status: 500,
+      statusText: 'Internal Server Error',
+    });
+
+    // getPrefectures関数を実行
+    const response: Prefecture[] | false = await getPrefectures();
+
+    // レスポンスがfalseであることを確認
+    expect(response).toBe(false);
+  });
 });
