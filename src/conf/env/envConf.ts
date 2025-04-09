62a9619f9eeb6aeefd480013ada75571fe63b175
kmjak <tymkoron1221@icloud.com>
2025-04-10 02:18:34 +0900

    chore: envConfでHOST_URLの環境変数を読んだ

---
+++ src/conf/env/envConf.ts
@@ -0,0 +1,14 @@
+import { EnvConf } from '@/types/env/EnvConf';
+
+/**
+ * @file envConf.ts
+ * @exports envConf
+ * @description: 環境設定に関する定数を定義する
+ *
+ * @author @kmjak
+ */
+
+export const envConf: EnvConf = {
+  // hostのURL
+  HOST_URL: process.env.NEXT_PUBLIC_HOST_URL || 'http://localhost:3000',
+};
