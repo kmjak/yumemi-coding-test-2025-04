d1f390e166c07037004dd0d042324516fe947ec1
kmjak <tymkoron1221@icloud.com>
2025-04-11 00:17:48 +0900

    refactor: エラーハンドリングの追加

--- src/usecases/api/populationComp/getPopulationCompByPrefCode.ts
+++ src/usecases/api/populationComp/getPopulationCompByPrefCode.ts
@@ -12,42 +12,52 @@ import { PopulationCompResponse } from '@/types/api/models/populationComp/Popula
  */
 
 export default async function getPopulationCompByPrefCode({ prefCode }: { prefCode: number }) {
-  // HOST_URLを取得
-  const { HOST_URL } = envConf;
+  try {
+    // HOST_URLを取得
+    const { HOST_URL } = envConf;
 
-  // HOST_URLが読み込めていない場合はエラーを投げる
-  if (HOST_URL === '') {
-    throw new Error('HOST_URL is not defined');
+    // HOST_URLが読み込めていない場合はエラーを投げる
+    if (HOST_URL === '') {
+      throw new Error('HOST_URL is not defined');
+    }
+
+    // API RouterのURLを作成
+    const apiRouterUrl: string = `${HOST_URL}/api/v1/population/comp`;
+
+    // APIの都道府県コードを指定して人口構成を取得する
+    const response: Response = await fetch(apiRouterUrl, {
+      // メソッドはPOST
+      method: 'POST',
+
+      // ヘッダーを指定
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      // リクエストボディを指定
+      body: JSON.stringify({ prefCode }),
+
+      // 都道府県コードは基本的に変わることがないのでキャッシュを使用
+      cache: 'force-cache',
+    });
+
+    // レスポンスがOKでない場合はエラーを投げる
+    if (!response.ok) {
+      throw new Error('Failed to fetch population composition');
+    }
+
+    // レスポンスをJSON形式で取得
+    const responseJson: PopulationCompResponse = await response.json();
+
+    // レスポンスのデータを返す
+    return responseJson;
+  } catch (error) {
+    if (error instanceof Error) {
+      // エラーがErrorインスタンスの場合は、そのままエラーメッセージを返す
+      throw error;
+    } else {
+      // エラーがErrorインスタンスでない場合は、Unknown errorを返す
+      throw new Error('Unknown error');
+    }
   }
-
-  // API RouterのURLを作成
-  const apiRouterUrl: string = `${HOST_URL}/api/v1/population/comp`;
-
-  // APIの都道府県コードを指定して人口構成を取得する
-  const response: Response = await fetch(apiRouterUrl, {
-    // メソッドはPOST
-    method: 'POST',
-
-    // ヘッダーを指定
-    headers: {
-      'Content-Type': 'application/json',
-    },
-
-    // リクエストボディを指定
-    body: JSON.stringify({ prefCode }),
-
-    // 都道府県コードは基本的に変わることがないのでキャッシュを使用
-    cache: 'force-cache',
-  });
-
-  // レスポンスがOKでない場合はエラーを投げる
-  if (!response.ok) {
-    throw new Error('Failed to fetch population composition');
-  }
-
-  // レスポンスをJSON形式で取得
-  const responseJson: PopulationCompResponse = await response.json();
-
-  // レスポンスのデータを返す
-  return responseJson;
 }
