c5ecb4053160a2b6e138b0b7f053bbb2bdfa32ec
kmjak <tymkoron1221@icloud.com>
2025-04-11 02:36:14 +0900

    feat: 人口構成に関するhandlerをまとめたカスタムフックを作った

--- src/hooks/api/usePopulationComp.ts
+++ src/hooks/api/usePopulationComp.ts
@@ -1,3 +1,6 @@
+import { PopulationCompResponse } from '@/types/api/models/populationComp/PopulationCompResponse';
+import getPopulationCompByPrefCode from '@/usecases/api/populationComp/getPopulationCompByPrefCode';
+
 /**
  * @file usePopulationComp.ts
  * @description 人口構成に関するhandlerをまとめたカスタムフック
@@ -5,3 +8,30 @@
  *
  * @author @kmjak
  */
+
+export default function usePopulationComp() {
+  /**
+   * 都道府県コードを指定して、人口構成の情報を取得するハンドラー
+   * @param prefCode 都道府県コード
+   * @returns 都道府県の人口構成情報
+   */
+  const handleGetPopulationCompByPrefCode = async ({
+    prefCode,
+  }: {
+    prefCode: number;
+  }): Promise<PopulationCompResponse | undefined> => {
+    try {
+      const populationCompResponse: PopulationCompResponse = await getPopulationCompByPrefCode({
+        prefCode,
+      });
+      return populationCompResponse;
+    } catch {
+      // エラーが発生した場合はundefinedを返す
+      return undefined;
+    }
+  };
+
+  return {
+    handleGetPopulationCompByPrefCode,
+  };
+}
