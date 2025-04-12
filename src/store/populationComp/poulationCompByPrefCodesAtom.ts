0f059bdc9272650760de929b127ceee68a4a6e6e
kmjak <tymkoron1221@icloud.com>
2025-04-13 04:00:44 +0900

    feat: 都道府県別の人口構成データを保持する状態を定義

---
+++ src/store/populationComp/poulationCompByPrefCodesAtom.ts
@@ -0,0 +1,4 @@
+import { PopulationCompByPrefCodes } from '@/types/models/populationComp/PopulationCompByPrefCodes';
+import { atom } from 'jotai';
+
+export const populationCompByPrefCodesAtom = atom<PopulationCompByPrefCodes>();
