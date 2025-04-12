c548706312f2324d5b6a792fc7a8c67aaeebac66
kmjak <tymkoron1221@icloud.com>
2025-04-13 05:20:47 +0900

    docs: 大まかなドキュメントを書いた

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,6 +1,16 @@
 import { JSX } from 'react';
 import cn from '@/utils/tailwind/cn';
 
+/**
+ * @file CheckboxのUIコンポーネント
+ * @description チェックボックスのUIコンポーネントを提供します。
+ * @param {string} className - 追加のクラス名
+ * @param {React.InputHTMLAttributes<HTMLInputElement>} props - その他のプロパティ
+ * @returns {JSX.Element} チェックボックスのUIコンポーネント
+ *
+ * @author @kmjak
+ */
+
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export default function Checkbox({ className, ...props }: CheckboxProps): JSX.Element {
