3dd2496e18cbe4ea96a1b94a476c5e0d05af2473
kmjak <tymkoron1221@icloud.com>
2025-04-13 05:31:07 +0900

    feat: 再利用可能なLabelのUIコンポーネントを作った

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,28 @@
+import { JSX } from 'react';
+import cn from '@/utils/tailwind/cn';
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  children: string | JSX.Element;
+}
+
+/**
+ * @file LabelのUIコンポーネント
+ * @description ラベルのUIコンポーネントを提供します。
+ * @param {string | JSX.Element} children - ラベルのメッセージ
+ * @param {string} className - 追加のクラス名
+ * @param {React.LabelHTMLAttributes<HTMLLabelElement>} props - その他のプロパティ
+ * @returns {JSX.Element} ラベルのUIコンポーネント
+ *
+ * @author @kmjak
+ */
+
+export default function Label({ children, className, ...props }: LabelProps) {
+  return (
+    <label
+      className={cn('text-gray-200 text-sm sm:text-base md:text-lg lg:text-xl peer', className)}
+      {...props}
+    >
+      {children}
+    </label>
+  );
+}
