9467e696d2a3b6f08d80720ac4279ee7e9c47947
kmjak <tymkoron1221@icloud.com>
2025-04-13 04:05:58 +0900

    docs: 都道府県番号別のBoundaryYearと人口構成を保持する状態のdocumentを書いた

--- src/store/populationComp/boundaryYearsAtom.ts
+++ src/store/populationComp/boundaryYearsAtom.ts
@@ -1,4 +1,11 @@
 import { BoundaryYears } from '@/types/models/populationComp/BoundaryYears';
 import { atom } from 'jotai';
 
+/**
+ * @file boundaryYearsAtom.ts
+ * @description 人口構成の区切り年を管理するatom
+ * @exports boundaryYearAtom
+ *
+ * @author @kmjak
+ */
 export const boundaryYearAtom = atom<BoundaryYears>();
