7bb3ec83ecc9c7423916937bb51a8cbd03058cb9
kmjak <tymkoron1221@icloud.com>
2025-04-13 18:58:43 +0900

    feat: 都道府県用のhooksを作った

---
+++ src/hooks/prefecture/usePrefecture.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState } from 'react';
+
+/**
+ * @file usePrefecture.ts
+ * @description 都道府県に関するhandlerをまとめたカスタムフック
+ * @returns {
+ *  checkedPrefectures: number[],
+ *  handleTogglePrefCode: ({ prefCode }: { prefCode: number }) => void,
+ * }
+ *
+ * @author @kmjak
+ */
+
+export default function usePrefecture(): {
+  checkedPrefectures: number[];
+  handleTogglePrefCode: ({ prefCode }: { prefCode: number }) => void;
+} {
+  const [checkedPrefectures, setCheckedPrefectures] = useState<number[]>([]);
+
+  /**
+   * @description 都道府県コードを受け取り、チェックボックスの状態のオンオフを切り替える
+   * @param {prefCode: number} - 都道府県コード
+   * @returns {void}
+   */
+  const handleTogglePrefCode = ({ prefCode }: { prefCode: number }): void => {
+    if (checkedPrefectures.includes(prefCode)) {
+      setCheckedPrefectures(checkedPrefectures.filter((code) => code !== prefCode));
+    } else {
+      setCheckedPrefectures([...checkedPrefectures, prefCode]);
+    }
+  };
+
+  return { checkedPrefectures, handleTogglePrefCode };
+}
