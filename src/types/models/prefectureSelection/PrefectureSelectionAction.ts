23cff32d516e2b29f42c9493bb5ce63ab4ef98d3
kmjak <tymkoron1221@icloud.com>
2025-04-15 00:22:22 +0900

    feat: checkboxの状態が変更された時のactionをグローバルで管理するAtomの型を定義した

---
+++ src/types/models/prefectureSelection/PrefectureSelectionAction.ts
@@ -0,0 +1,12 @@
+/**
+ * @file PrefectureSelectionAction.ts
+ * @description 都道府県選択のアクションを定義するインターフェース
+ * @exports PrefectureSelectionAction
+ *
+ * @author @kmjak
+ */
+
+export interface PrefectureSelectionAction {
+  action: 'insert' | 'delete';
+  prefCode: number;
+}
