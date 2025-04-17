3c157d2302a0cf97f549fcf5c7018fd16d826ff4
kmjak <tymkoron1221@icloud.com>
2025-04-18 03:07:01 +0900

    docs: ドキュメントの修正と追加

--- src/usecases/graphql/createRoom.ts
+++ src/usecases/graphql/createRoom.ts
@@ -1,16 +1,19 @@
 import { createYumemiCodingTest202504 } from '@/graphql/mutations';
 import { generateClient } from 'aws-amplify/api';
 
+/**
+ * @description createRoomの型定義
+ * @property {string} roomId - 部屋のID
+ */
 interface CreateRoomProps {
   roomId: string;
 }
 
 /**
  * @file createRoom.ts
- *
- * @description 都道府県コードを指定して、人口構成の情報を取得する関数
- * @param prefCode 都道府県コード
- * @return 都道府県の人口構成情報
+ * @description Appsyncを使って、dynamodbに部屋を登録する関数
+ * @param {CreateRoomProps} roomId - 部屋のID
+ * @returns {Promise<boolean>} - 成功した場合はtrue、失敗した場合はエラーを投げる
  *
  * @author @kmjak
  */
