a5e814840d5b16e8f3d45d2f0af4fb847cf4efb3
kmjak <tymkoron1221@icloud.com>
2025-04-18 04:37:16 +0900

    fix: codegenでGraphqlSchemaを再定義

--- src/types/models/graphql/GraphqlSchema.ts
+++ src/types/models/graphql/GraphqlSchema.ts
@@ -3,173 +3,173 @@
 //  This file was automatically generated and should not be edited.
 
 export type CreateYumemiCodingTest202504Input = {
-  roomId: string;
-  prefCodes?: Array<number | null> | null;
+  roomId: string,
+  prefCodes: Array< number | null >,
 };
 
 export type YumemiCodingTest202504 = {
-  __typename: 'YumemiCodingTest202504';
-  roomId: string;
-  prefCodes?: Array<number | null> | null;
+  __typename: "YumemiCodingTest202504",
+  roomId: string,
+  prefCodes: Array< number | null >,
 };
 
 export type UpdateYumemiCodingTest202504Input = {
-  roomId: string;
-  prefCodes?: Array<number | null> | null;
+  roomId: string,
+  prefCodes?: Array< number | null > | null,
 };
 
 export type DeleteYumemiCodingTest202504Input = {
-  roomId: string;
+  roomId: string,
 };
 
 export type TableYumemiCodingTest202504FilterInput = {
-  roomId?: TableStringFilterInput | null;
-  prefCodes?: TableIntFilterInput | null;
+  roomId?: TableStringFilterInput | null,
+  prefCodes?: TableIntFilterInput | null,
 };
 
 export type TableStringFilterInput = {
-  ne?: string | null;
-  eq?: string | null;
-  le?: string | null;
-  lt?: string | null;
-  ge?: string | null;
-  gt?: string | null;
-  contains?: string | null;
-  notContains?: string | null;
-  between?: Array<string | null> | null;
-  beginsWith?: string | null;
-  attributeExists?: boolean | null;
-  size?: ModelSizeInput | null;
+  ne?: string | null,
+  eq?: string | null,
+  le?: string | null,
+  lt?: string | null,
+  ge?: string | null,
+  gt?: string | null,
+  contains?: string | null,
+  notContains?: string | null,
+  between?: Array< string | null > | null,
+  beginsWith?: string | null,
+  attributeExists?: boolean | null,
+  size?: ModelSizeInput | null,
 };
 
 export type ModelSizeInput = {
-  ne?: number | null;
-  eq?: number | null;
-  le?: number | null;
-  lt?: number | null;
-  ge?: number | null;
-  gt?: number | null;
-  between?: Array<number | null> | null;
+  ne?: number | null,
+  eq?: number | null,
+  le?: number | null,
+  lt?: number | null,
+  ge?: number | null,
+  gt?: number | null,
+  between?: Array< number | null > | null,
 };
 
 export type TableIntFilterInput = {
-  ne?: number | null;
-  eq?: number | null;
-  le?: number | null;
-  lt?: number | null;
-  ge?: number | null;
-  gt?: number | null;
-  between?: Array<number | null> | null;
-  attributeExists?: boolean | null;
+  ne?: number | null,
+  eq?: number | null,
+  le?: number | null,
+  lt?: number | null,
+  ge?: number | null,
+  gt?: number | null,
+  between?: Array< number | null > | null,
+  attributeExists?: boolean | null,
 };
 
 export type YumemiCodingTest202504Connection = {
-  __typename: 'YumemiCodingTest202504Connection';
-  items?: Array<YumemiCodingTest202504 | null> | null;
-  nextToken?: string | null;
+  __typename: "YumemiCodingTest202504Connection",
+  items?:  Array<YumemiCodingTest202504 | null > | null,
+  nextToken?: string | null,
 };
 
 export type CreateYumemiCodingTest202504MutationVariables = {
-  input: CreateYumemiCodingTest202504Input;
+  input: CreateYumemiCodingTest202504Input,
 };
 
 export type CreateYumemiCodingTest202504Mutation = {
-  createYumemiCodingTest202504?: {
-    __typename: 'YumemiCodingTest202504';
-    roomId: string;
-    prefCodes?: Array<number | null> | null;
-  } | null;
+  createYumemiCodingTest202504?:  {
+    __typename: "YumemiCodingTest202504",
+    roomId: string,
+    prefCodes: Array< number | null >,
+  } | null,
 };
 
 export type UpdateYumemiCodingTest202504MutationVariables = {
-  input: UpdateYumemiCodingTest202504Input;
+  input: UpdateYumemiCodingTest202504Input,
 };
 
 export type UpdateYumemiCodingTest202504Mutation = {
-  updateYumemiCodingTest202504?: {
-    __typename: 'YumemiCodingTest202504';
-    roomId: string;
-    prefCodes?: Array<number | null> | null;
-  } | null;
+  updateYumemiCodingTest202504?:  {
+    __typename: "YumemiCodingTest202504",
+    roomId: string,
+    prefCodes: Array< number | null >,
+  } | null,
 };
 
 export type DeleteYumemiCodingTest202504MutationVariables = {
-  input: DeleteYumemiCodingTest202504Input;
+  input: DeleteYumemiCodingTest202504Input,
 };
 
 export type DeleteYumemiCodingTest202504Mutation = {
-  deleteYumemiCodingTest202504?: {
-    __typename: 'YumemiCodingTest202504';
-    roomId: string;
-    prefCodes?: Array<number | null> | null;
-  } | null;
+  deleteYumemiCodingTest202504?:  {
+    __typename: "YumemiCodingTest202504",
+    roomId: string,
+    prefCodes: Array< number | null >,
+  } | null,
 };
 
 export type GetYumemiCodingTest202504QueryVariables = {
-  roomId: string;
+  roomId: string,
 };
 
 export type GetYumemiCodingTest202504Query = {
-  getYumemiCodingTest202504?: {
-    __typename: 'YumemiCodingTest202504';
-    roomId: string;
-    prefCodes?: Array<number | null> | null;
-  } | null;
+  getYumemiCodingTest202504?:  {
+    __typename: "YumemiCodingTest202504",
+    roomId: string,
+    prefCodes: Array< number | null >,
+  } | null,
 };
 
 export type ListYumemiCodingTest202504sQueryVariables = {
-  filter?: TableYumemiCodingTest202504FilterInput | null;
-  limit?: number | null;
-  nextToken?: string | null;
+  filter?: TableYumemiCodingTest202504FilterInput | null,
+  limit?: number | null,
+  nextToken?: string | null,
 };
 
 export type ListYumemiCodingTest202504sQuery = {
-  listYumemiCodingTest202504s?: {
-    __typename: 'YumemiCodingTest202504Connection';
-    items?: Array<{
-      __typename: 'YumemiCodingTest202504';
-      roomId: string;
-      prefCodes?: Array<number | null> | null;
-    } | null> | null;
-    nextToken?: string | null;
-  } | null;
+  listYumemiCodingTest202504s?:  {
+    __typename: "YumemiCodingTest202504Connection",
+    items?:  Array< {
+      __typename: "YumemiCodingTest202504",
+      roomId: string,
+      prefCodes: Array< number | null >,
+    } | null > | null,
+    nextToken?: string | null,
+  } | null,
 };
 
 export type OnCreateYumemiCodingTest202504SubscriptionVariables = {
-  roomId?: string | null;
-  prefCodes?: Array<number | null> | null;
+  roomId?: string | null,
+  prefCodes?: Array< number | null > | null,
 };
 
 export type OnCreateYumemiCodingTest202504Subscription = {
-  onCreateYumemiCodingTest202504?: {
-    __typename: 'YumemiCodingTest202504';
-    roomId: string;
-    prefCodes?: Array<number | null> | null;
-  } | null;
+  onCreateYumemiCodingTest202504?:  {
+    __typename: "YumemiCodingTest202504",
+    roomId: string,
+    prefCodes: Array< number | null >,
+  } | null,
 };
 
 export type OnUpdateYumemiCodingTest202504SubscriptionVariables = {
-  roomId?: string | null;
-  prefCodes?: Array<number | null> | null;
+  roomId?: string | null,
+  prefCodes?: Array< number | null > | null,
 };
 
 export type OnUpdateYumemiCodingTest202504Subscription = {
-  onUpdateYumemiCodingTest202504?: {
-    __typename: 'YumemiCodingTest202504';
-    roomId: string;
-    prefCodes?: Array<number | null> | null;
-  } | null;
+  onUpdateYumemiCodingTest202504?:  {
+    __typename: "YumemiCodingTest202504",
+    roomId: string,
+    prefCodes: Array< number | null >,
+  } | null,
 };
 
 export type OnDeleteYumemiCodingTest202504SubscriptionVariables = {
-  roomId?: string | null;
-  prefCodes?: Array<number | null> | null;
+  roomId?: string | null,
+  prefCodes?: Array< number | null > | null,
 };
 
 export type OnDeleteYumemiCodingTest202504Subscription = {
-  onDeleteYumemiCodingTest202504?: {
-    __typename: 'YumemiCodingTest202504';
-    roomId: string;
-    prefCodes?: Array<number | null> | null;
-  } | null;
+  onDeleteYumemiCodingTest202504?:  {
+    __typename: "YumemiCodingTest202504",
+    roomId: string,
+    prefCodes: Array< number | null >,
+  } | null,
 };
