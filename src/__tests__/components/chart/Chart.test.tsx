d668d2fcb95cc5cfb75634f1111cef0a025fff76
kmjak <tymkoron1221@icloud.com>
2025-04-19 02:21:01 +0900

    test: Chartのテストコードを作った

---
+++ src/__tests__/components/chart/Chart.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import Chart from '@/components/chart/Chart';
+import { Prefecture } from '@/types/models/prefecture/Prefecture';
+
+/**
+ * @file Chart.test.tsx
+ * @description Chartコンポーネントのテストコード
+ */
+
+const mockPrefectures: Prefecture[] = [
+  { prefCode: 1, prefName: '北海道' },
+  { prefCode: 2, prefName: '青森県' },
+];
+
+describe('Chart', () => {
+  it('Chartコンポーネントが正しくレンダリングされる', () => {
+    render(<Chart chartMode="total" prefectures={mockPrefectures} />);
+
+    // チャートが正しくレンダリングされているか確認
+    expect(screen.getByText('都道府県を選択してください')).toBeInTheDocument();
+  });
+});
