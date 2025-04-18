9feb4804bf3d6a0c6f48699dc96289f9bae577d3
kmjak <tymkoron1221@icloud.com>
2025-04-19 01:55:08 +0900

    feat: ChartContainerのテストコードを作った

---
+++ src/__tests__/components/chart/ChartContainer.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import ChartContainer from '@/components/chart/ChartContainer';
+import { Prefecture } from '@/types/models/prefecture/Prefecture';
+
+jest.mock('@/components/chart/Chart', () => {
+  return function MockChart() {
+    return <div data-testid="chart" />;
+  };
+});
+jest.mock('@/components/chart/ChartModeSelector', () => {
+  return function MockChartModeSelector() {
+    return <div data-testid="chart-mode-selector" />;
+  };
+});
+
+const mockPrefectures: Prefecture[] = [
+  { prefCode: 1, prefName: '北海道' },
+  { prefCode: 2, prefName: '青森県' },
+];
+describe('ChartContainer', () => {
+  it('ChartModeSelectorとChartが正しくレンダリングされているか確認', () => {
+    render(<ChartContainer prefectures={mockPrefectures} />);
+
+    // ChartModeSelectorとChartが正しくレンダリングされているか確認
+    expect(screen.getByTestId('chart-mode-selector')).toBeInTheDocument();
+    expect(screen.getByTestId('chart')).toBeInTheDocument();
+  });
+});
