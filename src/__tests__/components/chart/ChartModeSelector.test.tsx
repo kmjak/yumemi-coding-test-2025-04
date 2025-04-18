d9fc1d22ab5334502d89248ac850e5286705c9c1
kmjak <tymkoron1221@icloud.com>
2025-04-19 02:12:21 +0900

    test: ChartModeSelectorのテストコードを書いた

---
+++ src/__tests__/components/chart/ChartModeSelector.test.tsx
@@ -0,0 +1,32 @@
+import ChartModeSelector from '@/components/chart/ChartModeSelector';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+describe('ChartModeSelector', () => {
+  /**
+   * テストケース: ChartModeSelectorが正しくレンダリングされているか確認
+   *
+   * @expect
+   * 正しくレンダリングされる
+   */
+  it('正しくレンダリングされるか確認', () => {
+    const setChartMode = jest.fn();
+    render(<ChartModeSelector setChartMode={setChartMode} />);
+    expect(screen.getByLabelText('モード選択')).toBeInTheDocument();
+  });
+
+  /**
+   * テストケース: モードを選択した時にsetChartModeが押され、'working'が渡されるか確認
+   *
+   * @expect
+   * setChartModeが呼ばれ、'working'が渡される
+   */
+  it('モードを選択した時にsetChartModeが押された時', () => {
+    const setChartMode = jest.fn();
+    render(<ChartModeSelector setChartMode={setChartMode} />);
+
+    const select = screen.getByLabelText('モード選択');
+    fireEvent.change(select, { target: { value: 'working' } });
+
+    expect(setChartMode).toHaveBeenCalledWith('working');
+  });
+});
